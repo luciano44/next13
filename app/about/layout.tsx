28481a562e27b5210cae145d6474f2128e04282f
luciano44 <luciano.ar6@gmail.com>
2023-10-25 23:57:15 -0300

    add about navbar

---
+++ app/about/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <nav>About Navbar</nav>
+      <main>{children}</main>
+    </>
+  )
+}
