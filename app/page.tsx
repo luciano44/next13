569cc1425f0a0e51f0a7a1e7593a992c150b78ad
luciano44 <luciano.ar6@gmail.com>
2023-10-29 23:49:43 -0300

    change default font

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image"
 import styles from "./page.module.css"
-import Button from "../components/button"
 import Link from "next/link"
+
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
-    <main>
-      <>
-        <h1>Home Page</h1>
-        <Link href="/about">About page</Link>
-      </>
-      <Button>Click Here</Button>
-      <Button>Do not Click Here</Button>
-      <Button>Click Here only if you want to</Button>
+    <main className={inter.className}>
+      <h1>Home Page</h1>
+      <p>
+        <Link href="/users">Users</Link>
+      </p>
     </main>
   )
 }
