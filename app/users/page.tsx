136515659abea8d598689722578f664714a0e1db
luciano44 <luciano.ar6@gmail.com>
2023-10-30 03:08:54 -0300

    create users and user page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,32 @@
 import type { Metadata } from "next"
+import getAllUsers from "@/lib/getAllUsers"
+import Link from "next/link"
 
 export const metadata: Metadata = {
   title: "Users",
 }
 
-export default function UsersPage() {
-  return <div>page</div>
+export default async function UsersPage() {
+  const usersData: Promise<User[]> = getAllUsers()
+
+  const users = await usersData
+
+  console.log("Hello")
+
+  return (
+    <section>
+      <h3>
+        <Link href="/home">Back to Home</Link>
+        <br />
+        {users.map((user) => (
+          <>
+            <p key={user.id}>
+              <Link href={`users/${user.id}`}>{user.name}</Link>
+            </p>
+            <br />
+          </>
+        ))}
+      </h3>
+    </section>
+  )
 }
