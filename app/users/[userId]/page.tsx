995467342af56185407de11e649fec077a7dc25d
luciano44 <luciano.ar6@gmail.com>
2023-10-31 04:15:45 -0300

    add Post type and create UserPosts component

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,9 +1,29 @@
+import getUser from "@/lib/getUser"
+import getUserPosts from "@/lib/getUserPosts"
+import { Suspense } from "react"
+import UserPosts from "./components/UserPosts"
+
 type Params = {
   params: {
     userId: string
   }
 }
 
-export default function UserPage({ params: { userId } }: Params) {
-  return <div>UserID: {userId} </div>
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId)
+  const userPostsData: Promise<Post[]> = getUserPosts(userId)
+
+  //const [user, userPosts] = await Promise.all([userData, userPostsData])
+
+  const user = await userData
+
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  )
 }
